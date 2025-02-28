ec3a195c1912b71d33cc3b8317f2a33f30e6de46
Mattias Verlinden <mattiverlinden@hotmail.com>
2025-02-28 15:32:27 +0100

    add isLoading function

--- src/pages/AllProducts.tsx
+++ src/pages/AllProducts.tsx
@@ -8,6 +8,7 @@ const AllProducts = () => {
   const [products, setProducts] = useState<Array<any>>([]);
   const [activeCategory, setActiveCategory] = useState<string>('all');
   const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
   const productsPerPage = 8;
 
   useEffect(() => {
@@ -16,6 +17,7 @@ const AllProducts = () => {
         const response = await fetch('https://fakestoreapi.com/products');
         const data = await response.json();
         setProducts(data);
+        setIsLoading(false);
       } catch (error) {
         console.error('Error fetching products:', error);
       }
@@ -47,80 +49,88 @@ const AllProducts = () => {
 
   return (
     <div className={styles.all_products}>
-      <h1>
-        {activeCategory === 'all'
-          ? 'All Products'
-          : `Products in ${activeCategory}`}
-      </h1>
-      <div className={styles.category_buttons}>
-        <button
-          type='button'
-          className={
-            activeCategory === 'all' ? styles.active_button : undefined
-          }
-          onClick={() => handleFilterChange('all')}
-        >
-          All
-        </button>
-        {Array.from(new Set(products.map((product) => product.category))).map(
-          (category) => (
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h1>
+            {activeCategory === 'all'
+              ? 'All Products'
+              : `Products in ${activeCategory}`}
+          </h1>
+          <div className={styles.category_buttons}>
             <button
-              key={category}
               type='button'
               className={
-                activeCategory === category ? styles.active_button : undefined
+                activeCategory === 'all' ? styles.active_button : undefined
               }
-              onClick={() => handleFilterChange(category)}
+              onClick={() => handleFilterChange('all')}
             >
-              {category}
+              All
             </button>
-          )
-        )}
-      </div>
-      <section className={styles.product_container}>
-        {currentProducts.map((product) => (
-          <section key={product.id} className={styles.product}>
-            <img src={product.image} alt={product.title} />
-            <button type='button'>
-              <IoCartOutline className={styles.cart_icon} />
-              Add to Cart
-            </button>
-            <h2>{product.title}</h2>
-            <p>${product.price}</p>
-            <p>
-              {[...Array(5)].map((_, i) => {
-                if (i < Math.floor(product.rating.rate)) {
-                  return <FaStar key={i} color='gold' />;
-                } else if (i === Math.floor(product.rating.rate)) {
-                  return <FaStarHalfAlt key={i} color='gold' />;
-                } else {
-                  return <FaRegStar key={i} color='grey' />;
+            {Array.from(
+              new Set(products.map((product) => product.category))
+            ).map((category) => (
+              <button
+                key={category}
+                type='button'
+                className={
+                  activeCategory === category ? styles.active_button : undefined
                 }
-              })}
-              <span>({product.rating.count})</span>
-            </p>
-            <Link to={`/product/${product.id}`}>
-              <FaRegEye className={styles.eye_icon} />
-            </Link>
+                onClick={() => handleFilterChange(category)}
+              >
+                {category}
+              </button>
+            ))}
+          </div>
+          <section className={styles.product_container}>
+            {currentProducts.map((product) => (
+              <section key={product.id} className={styles.product}>
+                <img src={product.image} alt={product.title} />
+                <button type='button'>
+                  <IoCartOutline className={styles.cart_icon} />
+                  Add to Cart
+                </button>
+                <h2>{product.title}</h2>
+                <p>${product.price}</p>
+                <p>
+                  {[...Array(5)].map((_, i) => {
+                    if (i < Math.floor(product.rating.rate)) {
+                      return <FaStar key={i} color='gold' />;
+                    } else if (i === Math.floor(product.rating.rate)) {
+                      return <FaStarHalfAlt key={i} color='gold' />;
+                    } else {
+                      return <FaRegStar key={i} color='grey' />;
+                    }
+                  })}
+                  <span>({product.rating.count})</span>
+                </p>
+                <Link to={`/product/${product.id}`}>
+                  <FaRegEye className={styles.eye_icon} />
+                </Link>
+              </section>
+            ))}
           </section>
-        ))}
-      </section>
-      <div className={styles.pagination}>
-        {Array.from(
-          { length: Math.ceil(filteredProducts.length / productsPerPage) },
-          (_, i) => (
-            <button
-              key={i + 1}
-              className={
-                currentPage === i + 1 ? styles.active_button : undefined
-              }
-              onClick={() => paginate(i + 1)}
-            >
-              {i + 1}
-            </button>
-          )
-        )}
-      </div>
+          <div className={styles.pagination}>
+            {Array.from(
+              {
+                length: Math.ceil(filteredProducts.length / productsPerPage),
+              },
+              (_, i) => (
+                <button
+                  key={i + 1}
+                  className={
+                    currentPage === i + 1 ? styles.active_button : undefined
+                  }
+                  onClick={() => paginate(i + 1)}
+                >
+                  {i + 1}
+                </button>
+              )
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
