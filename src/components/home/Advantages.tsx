a49e56ab2d5757ae18ffc8bd93aab31e157c455d
ViktoriiaIsk <vika.iskrich123@gmail.com>
2025-03-17 22:44:00 +0100

    Added css, imported to component

--- src/components/home/Advantages.tsx
+++ src/components/home/Advantages.tsx
@@ -1,16 +1,20 @@
 import { FaShippingFast, FaHeadset, FaUndo } from 'react-icons/fa';
+import styles from '../../styles/home/advantages.module.css';
 
 const advantages = [
-  { icon: <FaShippingFast />, title: 'Free and Fast Delivery', text: 'Free delivery for all orders over $140' },
-  { icon: <FaHeadset />, title: '24/7 Customer Service', text: 'Friendly 24/7 customer support' },
-  { icon: <FaUndo />, title: 'Money Back Guarantee', text: 'We return money within 30 days' }
+  { icon: <FaShippingFast className={styles.icon} />, title: "Free and Fast Delivery", text: "Free delivery for all orders over $140" },
+  { icon: <FaHeadset className={styles.icon} />, title: "24/7 Customer Service", text: "Friendly 24/7 customer support" },
+  { icon: <FaUndo className={styles.icon} />, title: "Money Back Guarantee", text: "We return money within 30 days" }
 ];
+
 const Advantages = () => {
   return (
-    <section>
+    <section className={styles.advantages}>
       {advantages.map((adv, index) => (
-        <div key={index}>
-          <div>{adv.icon}</div>
+        <div key={index.toString()} className={styles.advantage}>
+          <div className={styles.iconContainer}>
+            <div className={styles.innerCircle}>{adv.icon}</div>
+          </div>
           <h3>{adv.title}</h3>
           <p>{adv.text}</p>
         </div>
