eedb4727f768f1409b160fc012da24621ca0e699
ViktoriiaIsk <vika.iskrich123@gmail.com>
2025-03-19 10:28:34 +0100

    added ExploreProducts component to HomePage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,7 @@
 import CategorySection from '../components/home/CategorySection';
 // import Carousel from '../components/global/Carousel';
-import Advantages from '../components/home/Advantages';
-import AllProducts from '../pages/AllProducts'; 
-
+import Advantages from '../components/home/Advantages'; 
+import ExploreProducts from '../components/home/ExploreProducts';
 const HomePage = () => {
   return (
     <div>
@@ -11,7 +10,7 @@ const HomePage = () => {
 {/*      
       <Carousel />  */}
      
-      <AllProducts />
+     <ExploreProducts />
      
       <Advantages />
     </div>
