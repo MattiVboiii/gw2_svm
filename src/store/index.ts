e675bb73584b72044d1a86383ca32305afe69b48
ViktoriiaIsk <vika.iskrich123@gmail.com>
2025-03-23 15:59:03 +0100

    Add FlashSales section using centralized Redux products and reusable ProductCard with complete styles

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import {thunk} from "redux-thunk"; // Add redux-thunk for async actions
 import logger from "redux-logger";
 import productsSlice from "./productsSlice";
 
 const rootReducer = combineReducers({
-  productsSlice: productsSlice,
+  productsSlice,
 });
+
 export type RootState = ReturnType<typeof rootReducer>;
-const store = createStore(rootReducer, applyMiddleware(logger));
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger)); // Apply thunk and logger
 
 export default store;
